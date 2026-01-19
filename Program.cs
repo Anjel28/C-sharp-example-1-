f8559d00c14f261b44363e429288f632b9584ad9
Anjel28 <anjelchaudhary28@gmail.com>
2026-01-19 20:40:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+class Program
+{
+    static void Main()
+    {
+
+   char studentGrade = 'A';
+
+   Console.WriteLine("Student Grade: " +studentGrade);
+       /* bool isLightOn = true;
+
+        Console.WriteLine("Light Status: " +isLightOn);*/
+
+
+   /*int temperature = 25 ;
+
+   Console.WriteLine("Temperature:  " +temperature +"degree celsius");*/
+        /*double bankBalance = 1500.75;
+
+        Console.WriteLine("Bank Balance: " +bankBalance);*/
+
+
+  /* string studentName = "Rahul";
+   int studentAge = 20;
+
+   Console.WriteLine("studentName: " +studentName);
+   Console.WriteLine("studentAge: " +studentAge);*/        
+    }
+}
